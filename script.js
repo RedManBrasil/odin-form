81342546423c6691a705a87f5422e4bd84689770
RedManBrasil <pefandrade@hotmail.com>
2022-11-03 18:39:47 -0300

    Improved UI 0.4

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-let passwords_equal = false; //variavel que acompanha se as senhas são iguais
-
 let pwd_1input = "";
 let pwd_2input = "";
 
 const error_message = document.querySelector('#error_password');
+const submit_btn = document.querySelector('#submit_button');
+
+submit_btn.disabled = true; //the submit button starts disabled
 
 const password_op = document.querySelector('#password'); 
 password_op.addEventListener("change", (a) => { 
     pwd_1input = a.target.value;
     if (pwd_2input != ""){
         if (pwd_1input != pwd_2input){
+            submit_btn.disabled = true;
             error_message.classList.remove("invisible");
         } else{
+            submit_btn.disabled = false;
             error_message.classList.add("invisible");
         }
     }
@@ -21,8 +24,10 @@ const ConfirmPassword_op = document.querySelector('#confirm-password');
 ConfirmPassword_op.addEventListener("change", (a) => { 
     pwd_2input = a.target.value;
     if (pwd_1input != pwd_2input){
+        submit_btn.disabled = true;
         error_message.classList.remove("invisible");
     } else{
+        submit_btn.disabled = false;
         error_message.classList.add("invisible");
     }
 });
